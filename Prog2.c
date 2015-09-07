b2b8b447bef859c5d424d24984a10ea2572f444a
cloud95 <all418@lehigh.edu>
2015-09-07 16:24:35 -0400

    Create Prog2.c

---
+++ Prog2.c
@@ -0,0 +1,21 @@
+/*                                                                              
+  CSE 109                                                                       
+  Allan Lee                                                                     
+  all418                                                                        
+  Program Description:                                                          
+  Program #2                                                                    
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <uninstd.h>
+#include <sys/wait.h>
+
+char **	getopts(char cmd[]);
+
+int main(int argc, char	*argv[]){
+  string introCommand =	"Please enter the name of the host computer followed by\
+ the >";
+
+}
+
